05499e566f255f1ef17fd1040fcbfd77abbfb106
wgq1218 <wgq1218@gmail.com>
2021-08-23 23:47:30 +0800

    改bug

--- djtester/base_repositories.py
+++ djtester/base_repositories.py
@@ -7,31 +7,40 @@ class BaseDBHelper:
         # 由于model.objects必须用本来的model来调用,所以import对应的model
         models = importlib.import_module(model_path)
         self.model = getattr(models, model_name)
+        self.entity = self.model()
         if data:
             self.entity = self.model(**data)
+        # 取出所有field的name以后用
+        self.fields_name = []
+        for f in self.entity._meta.fields:
+            self.fields_name.append(f.name)
 
     @transaction.atomic
     def save_this(self):
         # 通过pk判断是否新增
         if self.entity.pk:
             # 有pk判断为修改已存在的数据
-            try:
-                self.entity = self.model.objects.get(pk=self.entity.pk)
-            except Exception as e:
-                raise Exception(f'从 {self.model.__name__} 查询 id为 {self.entity.pk} 的数据报错: {e}')
-            # 必须先取出不为空的字段,再传入修改,还要排除id字段
-            entity_dict: dict = self.entity.fields_dict()
-            for (k, v) in entity_dict.items():
-                if v is None or k in ['id']:
-                    entity_dict.pop(k)
-                else:
-                    continue
-            self.entity.save(update_fields=entity_dict.keys())
+            # 修改前需要获取正确的update_fields
+            update_fields = self._get_update_fields()
+            self.entity.save(update_fields=update_fields)
             return self.entity
         else:
             # 没有pk判断为新增
             self.entity.save()
             return self.entity
+
+    def _get_update_fields(self):
+        # 1,排除值为None的字段
+        # 2,排除id字段
+        entity_dict: dict = self.entity.__dict__
+        for (k, v) in entity_dict.items():
+            if v is None:
+                self.fields_name.remove(k)
+            else:
+                continue
+        if 'id' in self.fields_name:
+            self.fields_name.remove('id')
+        return self.fields_name
 
     def get_all(self, offset: int, limit: int):
         return self.model.objects.all()[offset: (offset + limit)]
