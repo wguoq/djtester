46c11e71ae190cf28041bf5f0ee94ae7a7fa140c
wgq1218 <wgq1218@gmail.com>
2021-08-24 15:41:40 +0800

    改bug

--- djtester/base_repositories.py
+++ djtester/base_repositories.py
@@ -4,43 +4,43 @@ from django.db import transaction
 
 class BaseDBHelper:
     def __init__(self, model_path: str, model_name: str, data: dict = None):
+        self.data = data
         # 由于model.objects必须用本来的model来调用,所以import对应的model
         models = importlib.import_module(model_path)
         self.model = getattr(models, model_name)
         self.entity = self.model()
-        if data:
-            self.entity = self.model(**data)
-        # 取出所有field的name以后用
+        if self.data:
+            self.entity = self.model(**self.data)
+        # 取出所有field.name以后用
         self.fields_name = []
         for f in self.entity._meta.fields:
             self.fields_name.append(f.name)
 
     @transaction.atomic
     def save_this(self):
-        # 通过pk判断是否新增
-        if self.entity.pk:
-            # 有pk判断为修改已存在的数据
-            # 修改前需要获取正确的update_fields
-            update_fields = self._get_update_fields()
-            self.entity.save(update_fields=update_fields)
-            return self.entity
+        pk = self.entity.pk
+        if pk:
+            # 有pk判断为修改
+            # 先把原始数据查出来
+            original = self.model.objects.get(pk=pk).__dict__
+            # 把新数据的内容写进来,只写不为None的字段
+            for field in self.fields_name:
+                if self.data.get(field):
+                    original[field] = self.data.get(field)
+                else:
+                    continue
+            # 根据fields_name过滤出数据
+            new = {}
+            for field in self.fields_name:
+                new.update({field: original[field]})
+            # 生成新的实体保存
+            new_entity = self.model(**new)
+            new_entity.save()
+            return new_entity
         else:
             # 没有pk判断为新增
             self.entity.save()
             return self.entity
-
-    def _get_update_fields(self):
-        # 1,排除值为None的字段
-        # 2,排除id字段
-        entity_dict: dict = self.entity.__dict__
-        for (k, v) in entity_dict.items():
-            if v is None:
-                self.fields_name.remove(k)
-            else:
-                continue
-        if 'id' in self.fields_name:
-            self.fields_name.remove('id')
-        return self.fields_name
 
     def get_all(self, offset: int, limit: int):
         return self.model.objects.all()[offset: (offset + limit)]
