eb65860efd8118eca828a7d012a44a42e1795a64
wgq1218 <wgq1218@gmail.com>
2021-08-30 11:20:47 +0800

    把save m2m的写法也改了

--- testcase/domain/tc_repositories.py
+++ testcase/domain/tc_repositories.py
@@ -1,12 +1,17 @@
 import importlib
 
+from django.db import transaction
+
+from djtester.base_repositories import BaseDBHelper
 from testcase.models import Test_Case
-from testcase.repositories import *
+from testcase.repositories import TcIdentityDBHelper
+
 MODELS_PATH = 'testcase.models'
 
 helper = {'tc_identity': 'TcIdentityDBHelper',
           'tc_action': 'TcActionDBHelper',
-          'tc_data': 'TcDataDBHelper'}
+          'tc_data': 'TcDataDBHelper',
+          'tc_check_point': 'TcCheckPointDBHelper'}
 
 
 def _save_foreignkey(foreignkey_name, data):
@@ -25,7 +30,7 @@ def _save_foreignkey(foreignkey_name, data):
 
 class TcTestCaseDBHelper(BaseDBHelper):
     """
-    data可以是有字段数据的dict也可也是已存在数据的id
+    data里面的外键字段可以是dict也可以是id
     """
     def __init__(self, data: dict = None):
         super().__init__(MODELS_PATH, Test_Case.__name__, None)
@@ -35,54 +40,30 @@ class TcTestCaseDBHelper(BaseDBHelper):
             self.tc_action = self.data.get('tc_action')
             self.tc_data = self.data.get('tc_data')
             self.tc_check_list = self.data.get('tc_check_list')
+            # tc_check_list是m2m要先移除
             if self.tc_check_list:
                 self.data.pop('tc_check_list')
         else:
             self.data = {}
 
     def _set_foreignkey(self):
-        # 需要处理数据是dict或者id的情况
-        # if self.tc_identity is None:
-        #     self.data['tc_identity'] = None
-        # elif isinstance(self.tc_identity, dict):
-        #     self.data['tc_identity'] = TcIdentityDBHelper(self.tc_identity).save_this()
-        # else:
-        #     self.data['tc_identity'] = TcIdentityDBHelper().get_by(dict(pk=self.tc_identity))
-        #
-        # if self.tc_action is None:
-        #     self.data['tc_action'] = None
-        # elif isinstance(self.tc_action, dict):
-        #     self.data['tc_action'] = TcActionDBHelper(self.tc_action).save_this()
-        # else:
-        #     self.data['tc_action'] = TcActionDBHelper().get_by(dict(pk=self.tc_action))
-        #
-        # if self.tc_data is None:
-        #     self.data['tc_data'] = None
-        # elif isinstance(self.tc_data, dict):
-        #     self.data['tc_data'] = TcDataDBHelper(self.tc_data).save_this()
-        # else:
-        #     self.data['tc_data'] = TcDataDBHelper().get_by(dict(pk=self.tc_data))
         self.data['tc_identity'] = _save_foreignkey('tc_identity', self.tc_identity)
         self.data['tc_action'] = _save_foreignkey('tc_action', self.tc_action)
         self.data['tc_data'] = _save_foreignkey('tc_data', self.tc_data)
 
-        return self.data
-
     def _set_m2m(self):
-        new_check_list = []
         if self.tc_check_list is None:
             return None
         elif len(self.tc_check_list) == 0:
+            return []
+        elif isinstance(self.tc_check_list, list):
+            new_check_list = []
+            for check in self.tc_check_list:
+                a = _save_foreignkey('tc_check_point', check)
+                new_check_list.append(a)
             return new_check_list
         else:
-            for check in self.tc_check_list:
-                if isinstance(check, dict):
-                    new_check = TcCheckPointDBHelper(check).save_this()
-                    new_check_list.append(new_check)
-                else:
-                    new_check = TcCheckPointDBHelper().get_by(dict(pk=check))
-                    new_check_list.append(new_check)
-            return new_check_list
+            raise Exception(f'tc_check_list 需要是None或者list类型')
 
     # m2m的字段要单独处理
     # 事务
@@ -92,7 +73,7 @@ class TcTestCaseDBHelper(BaseDBHelper):
         self._set_foreignkey()
         # 存m2m
         m2m = self._set_m2m()
-        # 通过pk判断是否新增
+        # 通过pk判断是不是修改
         pk = self.data.get('id')
         if pk:
             # 修改前先排除值为None的字段
@@ -106,6 +87,7 @@ class TcTestCaseDBHelper(BaseDBHelper):
                 new.tc_check_list.set(m2m)
             return new
         else:
+            # 新增
             new = Test_Case(**self.data)
             new.save()
             if m2m:
@@ -114,10 +96,10 @@ class TcTestCaseDBHelper(BaseDBHelper):
 
     @staticmethod
     def filter_by_case_id(test_case_id):
-        ide = TcIdentityDBHelper().get_by(dict(test_case_id=test_case_id))
-        return Test_Case.objects.filter(tc_identity=ide)
+        tc_identity_id = TcIdentityDBHelper().get_by(dict(test_case_id=test_case_id)).pk
+        return Test_Case.objects.filter(tc_identity=tc_identity_id)
 
     @staticmethod
     def filter_by_case_name(test_case_name):
-        ide = TcIdentityDBHelper().get_by(dict(test_case_name=test_case_name))
-        return Test_Case.objects.filter(tc_identity=ide)
+        tc_identity_id = TcIdentityDBHelper().get_by(dict(test_case_name=test_case_name)).pk
+        return Test_Case.objects.filter(tc_identity=tc_identity_id)
