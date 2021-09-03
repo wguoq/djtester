492c1039bef68caf382a3a417fd5bafb98c2515f
wgq1218 <wgq1218@gmail.com>
2021-09-03 17:49:21 +0800

    删除不要的代码

--- djtester/base_repositories.py
+++ djtester/base_repositories.py
@@ -12,33 +12,10 @@ class BaseDBHelper:
         if self.data:
             self.entity = self.model(**self.data)
 
-        # 取出所有field.name以后用
-        # self.fields_name = []
-        # for f in self.entity._meta.fields:
-        #     self.fields_name.append(f.name)
-
-
     @transaction.atomic
     def save_this(self):
         pk = self.entity.pk
         if pk:
-            # 有pk判断为修改
-            # 先把原始数据查出来
-            # original = self.model.objects.get(pk=pk).__dict__
-            # # 把新数据的内容写进来,只写不为None的字段
-            # for field in self.fields_name:
-            #     if self.data.get(field):
-            #         original[field] = self.data.get(field)
-            #     else:
-            #         continue
-            # # 根据fields_name过滤出数据
-            # new = {}
-            # for field in self.fields_name:
-            #     new.update({field: original[field]})
-            # # 生成新的实体保存
-            # new_entity = self.model(**new)
-            # new_entity.save()
-            # return new_entity
             self.model.objects.filter(pk=pk).update(**self.data)
             return self.model.objects.get(pk=pk)
         else:
@@ -54,4 +31,3 @@ class BaseDBHelper:
 
     def filter_by(self, kwargs):
         return self.model.objects.filter(**kwargs)
-
