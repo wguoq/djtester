00cc0afddd63716fd0cce50b7ef3f4b4be5a0ea7
wgq1218 <wgq1218@gmail.com>
2021-10-24 23:15:50 +0800

    1

--- tester/views.py
+++ tester/views.py
@@ -11,3 +11,15 @@ def run_test_case(request):
     data = {"test_case_pk": test_case_pk,
             "result": result}
     return JsonResponse(data, status=200)
+
+
+def run_test_case_list(request):
+    test_case_list = request.POST.get('test_case_list')
+    data_list = []
+    # 多线程 todo
+    for test_case_pk in test_case_list:
+        result = TesterServicer(test_case_pk).run_testcase().test_case_result.to_dict()
+        data = {"test_case_pk": test_case_pk,
+                "result": result}
+        data_list.append(data)
+    return JsonResponse(data_list, status=200)
