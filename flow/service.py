f96f9bec9b0a3eb893264a732e16d58f8e764c8e
wgq1218 <wgq1218@gmail.com>
2021-10-27 09:04:09 +0800

    1

--- flow/service.py
+++ flow/service.py
@@ -9,10 +9,10 @@ class NodeFuncRunFLow(NodeFuncBase):
     def __init__(self):
         super().__init__()
 
-    def do_func(self, node_func_data: dict, flow_data):
+    def do_func(self, node_func_data: dict, flow_data: dict):
         flow_design_id = node_func_data.get('flow_design_id')
         flow_design = FlowDesignDBHelper().get_by({'pk': flow_design_id})
-        flow_instance = FlowMgr().instance_flow_design(flow_design)
+        flow_instance = FlowMgr().instance_flow_design(flow_design, flow_data)
         a = FlowMgr().run_flow(flow_instance)
         new_flow_instance: Flow_Instance = a.new_flow_instance
         self.result = new_flow_instance.flow_result
