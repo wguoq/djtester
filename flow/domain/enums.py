eceb70657abc1a7dd84cc5dd4da374765ccb2ba7
wgq1218 <wgq1218@gmail.com>
2021-12-22 17:41:00 +0800

    新增2个枚举类

--- flow/domain/enums.py
+++ flow/domain/enums.py
@@ -29,3 +29,13 @@ class NodeStartRuleTarget(Enum):
     FlowData = 'flow_data'
     NodeResult = 'node_result'
     NodeStatus = 'node_status'
+
+
+class FlowResultRuleType(Enum):
+    Default_1 = ['last_node_result', '使用最后一个运行过的节点结果']
+    Custom = ['custom', '使用自定义方法']
+
+
+class FlowStatusRuleType(Enum):
+    Default_1 = ['last_node_status', '使用最后一个运行过的节点状态']
+    Custom = ['custom', '使用自定义方法']
