2c1d147f8e54aa6d36d082cb5032800f3560510d
wgq1218 <wgq1218@gmail.com>
2022-01-19 18:37:28 +0800

    修改run_test_case返回数据

--- tester/views.py
+++ tester/views.py
@@ -2,15 +2,16 @@ import json
 
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
 from tester.service import TesterServicer
 
 
+@csrf_exempt
 def run_test_case(request):
-    test_case_id = request.POST.get('test_case_id')
-    result = TesterServicer.run_testcase(test_case_id).test_case_result
-    data = {"test_case_id": test_case_id,
-            "result": result}
-    return JsonResponse(data, status=200)
+    test_case_id = request.POST.get('id')
+    result = TesterServicer.run_testcase(test_case_id)
+    return JsonResponse(status=200, data=result.__dict__)
 
 
 def run_test_case_list(request):
@@ -20,7 +21,6 @@ def run_test_case_list(request):
     for test_case in test_case_list:
         test_case_id = test_case.get('id')
         result = TesterServicer.run_testcase(test_case_id).test_case_result
-        data = {"test_case_id": test_case_id,
-                "result": result}
+        data = dict(test_case_id=test_case_id, result=result)
         data_list.append(data)
-    return JsonResponse(data_list, status=200)
+    return JsonResponse(status=200, data=data_list)
