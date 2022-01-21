3d66b08cf0e396b93728479e94896442c2563c9b
wgq1218 <wgq1218@gmail.com>
2022-01-21 17:57:42 +0800

    修改返回格式

--- tester/views.py
+++ tester/views.py
@@ -4,14 +4,15 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 
-from tester.service import TesterServicer
+from tester.service import TesterService
 
 
 @csrf_exempt
 def run_test_case(request):
     test_case_id = request.POST.get('id')
-    result = TesterServicer.run_testcase(test_case_id)
-    return JsonResponse(status=200, data=result.__dict__)
+    result = TesterService.run_testcase(test_case_id)
+    context = dict(data=result.__dict__)
+    return JsonResponse(status=200, data=context)
 
 
 def run_test_case_list(request):
@@ -20,7 +21,7 @@ def run_test_case_list(request):
     # 多线程 todo
     for test_case in test_case_list:
         test_case_id = test_case.get('id')
-        result = TesterServicer.run_testcase(test_case_id).test_case_result
-        data = dict(test_case_id=test_case_id, result=result)
-        data_list.append(data)
-    return JsonResponse(status=200, data=data_list)
+        result = TesterService.run_testcase(test_case_id)
+        data_list.append(result.__dict__)
+    context = dict(data=data_list)
+    return JsonResponse(status=200, data=context)
