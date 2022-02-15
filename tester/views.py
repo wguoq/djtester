6fac77b9c48fb217a0e681f35e67bdf397406a6c
wgq1218 <wgq1218@gmail.com>
2022-02-15 22:45:20 +0800

    增加commit接口

--- tester/views.py
+++ tester/views.py
@@ -1,15 +1,26 @@
+import importlib
 import json
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 
 from tester.service import TesterService
 
 
-@csrf_exempt
 def run_test_case(request):
     test_case_id = request.POST.get('id')
+    result = TesterService.run_testcase(test_case_id)
+    context = dict(data=result.__dict__)
+    return JsonResponse(status=200, data=context)
+
+
+def run(request):
+    print(request)
+    print(request.body)
+    j = json.loads(request.body)
+    print(j)
+    test_case_id = j.get('id')
     result = TesterService.run_testcase(test_case_id)
     context = dict(data=result.__dict__)
     return JsonResponse(status=200, data=context)
@@ -25,3 +36,28 @@ def run_test_case_list(request):
         data_list.append(result.__dict__)
     context = dict(data=data_list)
     return JsonResponse(status=200, data=context)
+
+
+tester_service = importlib.import_module('tester.service')
+
+
+def commit(request):
+    if request.method != 'POST':
+        return HttpResponseNotFound
+    else:
+        context = {}
+        payload = json.loads(request.body)
+        print(request)
+        print(payload)
+        service_name = payload.get('service')
+        service = getattr(tester_service, service_name)
+        print(f"service= {service}")
+        action = payload.get('action')
+        data = payload.get('data')
+        if action == "run":
+            test_case_id = data.get('id')
+            result = TesterService.run_testcase(test_case_id)
+            context = dict(data=result.__dict__)
+            return JsonResponse(status=200, data=context)
+        else:
+            return JsonResponse(context, status=200)
