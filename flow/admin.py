1b139d30d0bb5dd80800edfa9af5fe9b17bb5998
wgq1218 <wgq1218@gmail.com>
2022-02-15 22:39:39 +0800

    管理页面修改

--- flow/admin.py
+++ flow/admin.py
@@ -8,20 +8,35 @@ from .models import *
 
 class Flow_Design_Admin(admin.ModelAdmin):
     list_display = (
-        'flow_code', 'flow_name', 'flow_type', 'flow_result_rule_id', 'flow_status_rule_id', 'version',
+        'id', 'flow_code', 'flow_name', 'flow_type', 'flow_result_rule_id', 'flow_status_rule_id', 'version',
         'version_status')
 
 
 class Node_Design_Admin(admin.ModelAdmin):
     list_display = (
-        'node_code', 'node_name', 'node_type', 'start_rule_type', 'node_func_name', 'node_func_data', 'version',
+        'id', 'node_code', 'node_name', 'node_type', 'start_rule_type', 'node_func_name', 'node_func_data', 'version',
         'version_status')
 
+
+class Flow_Node_Design_Oder_Admin(admin.ModelAdmin):
+    list_display = (
+        'id', 'flow_design', 'node_order', 'node_design')
+
+
+class Flow_Instance_Admin(admin.ModelAdmin):
+    list_display = (
+        'id', 'flow_design', 'flow_data', 'flow_status', 'flow_result')
+
+class Node_Instance_Admin(admin.ModelAdmin):
+    list_display = (
+        'id', 'node_design', 'node_func_name', 'node_func_data', 'node_order','node_status','node_result','flow_instance')
 
 admin.site.register(Flow_Design, Flow_Design_Admin)
 admin.site.register(Flow_Result_Rule)
 admin.site.register(Flow_Status_Rule)
-admin.site.register(Node_Design)
+admin.site.register(Node_Design, Node_Design_Admin)
 admin.site.register(Node_Start_Rule)
 admin.site.register(Node_Status_Rule)
-admin.site.register(Flow_Node_Design_Oder)
+admin.site.register(Flow_Node_Design_Oder, Flow_Node_Design_Oder_Admin)
+admin.site.register(Flow_Instance, Flow_Instance_Admin)
+admin.site.register(Node_Instance, Node_Instance_Admin)
