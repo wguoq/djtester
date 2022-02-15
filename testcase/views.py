2fdcc0b051413e73865c115fb189dce88dbc731d
wgq1218 <wgq1218@gmail.com>
2022-02-15 22:44:27 +0800

    增加query接口

--- testcase/views.py
+++ testcase/views.py
@@ -1,3 +1,4 @@
+import importlib
 import json
 
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
@@ -41,3 +42,34 @@ def get_all(request):
         total = len(all_case)
         context = {'data': all_case, 'total': total}
         return JsonResponse(context, status=200)
+
+
+tc_service = importlib.import_module('testcase.service')
+
+
+def query(request):
+    if request.method != 'GET':
+        return HttpResponseNotFound
+    else:
+        context = {}
+        params = request.GET
+        print(f"params= {params}")
+        if params and len(params) >= 1:
+            # 这些取出来以后是str,可以用json.loads转一下，但是必须保证里面都是双引号
+            service_name = params.get('service')
+            action = params.get('action')
+            filters = json.loads(params.get('filters'))
+            page = json.loads(params.get('page'))
+            pageable = page.get('pageable')
+            pageSize = page.get('pageSize')
+            pageNumber = page.get('pageNumber')
+            service = getattr(tc_service, service_name)
+            if action == 'all':
+                result = service().get_all()
+                context = dict(rows=result, total=len(result))
+            elif action == 'get':
+                pass
+            elif action == 'filter':
+                pass
+            return JsonResponse(context, status=200)
+
