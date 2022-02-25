3cd9bdf80893842785f56d7b3d5927791f37ffdf
wguoq <wgq1218@gmail.com>
2022-02-25 18:25:26 +0800

    commit增加对批量的支持

--- flow/views.py
+++ flow/views.py
@@ -69,43 +69,89 @@ def commit(request):
     if request.method != 'POST':
         return HttpResponseNotFound
     else:
-        context = {}
-        status = 200
         payload = json.loads(request.body)
         service_name = payload.get('service')
         service = getattr(flow_service, service_name)
         print(f"payload = {payload}")
         action = payload.get('action')
         data = payload.get('data')
-        if action == "instance":
-            try:
-                inst = service().instance_flow(data.get('id'), data.get('flow_data'))
-                context = dict(instance_id=inst.id)
-            except Exception as e:
-                # 直接打印异常
-                traceback.print_exc()
-                # 返回字符串
-                # traceback.format_exc()
-                # 还可以将信息写入到文件
-                # traceback.print_exc(file=open(‘error.txt’, ’a +’))
-                return JsonResponse(dict(message=str(e)), status=500)
-        elif action == "run":
-            try:
-                result = service().run_inst(data.get('id'))
-                context = dict(instance_id=result)
-            except Exception as e:
-                traceback.print_exc()
-                return JsonResponse(dict(message=str(e)), status=500)
-        elif action == 'add':
-            try:
-                context = service().add(data)
-            except Exception as e:
-                traceback.print_exc()
-                return JsonResponse(dict(message=str(e)), status=500)
-        elif action == 'edit':
-            try:
-                context = service().edit(data)
-            except Exception as e:
-                traceback.print_exc()
-                return JsonResponse(dict(message=str(e)), status=500)
-        return JsonResponse(context, status=status)
+        if isinstance(data, dict):
+            result = do_commit(service, action, data)
+            return JsonResponse(result.get('context'), status=result.get('status'))
+        elif isinstance(data, list):
+            result_list = []
+            for d in data:
+                result = do_commit(service, action, d)
+                result_list.append(result)
+            return JsonResponse(result_list, status=200)
+        # if action == "instance":
+        #     try:
+        #         inst = service().instance_flow(data.get('id'), data.get('flow_data'))
+        #         context = dict(instance_id=inst.id)
+        #     except Exception as e:
+        #         # 直接打印异常
+        #         traceback.print_exc()
+        #         # 返回字符串
+        #         # traceback.format_exc()
+        #         # 还可以将信息写入到文件
+        #         # traceback.print_exc(file=open(‘error.txt’, ’a +’))
+        #         return JsonResponse(dict(message=str(e)), status=500)
+        # elif action == "run":
+        #     try:
+        #         result = service().run_inst(data.get('id'))
+        #         context = dict(instance_id=result)
+        #     except Exception as e:
+        #         traceback.print_exc()
+        #         return JsonResponse(dict(message=str(e)), status=500)
+        # elif action == 'add':
+        #     try:
+        #         context = service().add(data)
+        #     except Exception as e:
+        #         traceback.print_exc()
+        #         return JsonResponse(dict(message=str(e)), status=500)
+        # elif action == 'edit':
+        #     try:
+        #         context = service().edit(data)
+        #     except Exception as e:
+        #         traceback.print_exc()
+        #         return JsonResponse(dict(message=str(e)), status=500)
+        # return JsonResponse(context, status=status)
+
+
+def do_commit(service, action, data):
+    if action == "instance":
+        try:
+            inst = service().instance_flow(data.get('id'), data.get('flow_data'))
+            context = dict(instance_id=inst.id)
+            return dict(context=context, status=200)
+        except Exception as e:
+            # 直接打印异常
+            traceback.print_exc()
+            # 返回字符串
+            # traceback.format_exc()
+            # 还可以将信息写入到文件
+            # traceback.print_exc(file=open(‘error.txt’, ’a +’))
+            return dict(context=dict(message=str(e)), status=500)
+    elif action == "run":
+        try:
+            result = service().run_inst(data.get('id'))
+            context = dict(instance_id=result)
+            return dict(context=context, status=200)
+        except Exception as e:
+            traceback.print_exc()
+            return dict(context=dict(message=str(e)), status=500)
+    elif action == 'add':
+        try:
+            context = service().add(data)
+            return dict(context=context, status=200)
+        except Exception as e:
+            traceback.print_exc()
+            return dict(context=dict(message=str(e)), status=500)
+    elif action == 'edit':
+        try:
+            context = service().edit(data)
+            return dict(context=context, status=200)
+        except Exception as e:
+            traceback.print_exc()
+            return dict(context=dict(message=str(e)), status=500)
+
