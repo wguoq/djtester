fc00f8d0d73f21009a803b9c20ec3c2aa7876bea
wguoq <wgq1218@gmail.com>
2022-03-03 17:20:51 +0800

    把时间显示出了

--- flow/admin.py
+++ flow/admin.py
@@ -9,29 +9,29 @@ from .models import *
 class Flow_Design_Admin(admin.ModelAdmin):
     list_display = (
         'id', 'code', 'fw_name', 'fw_type', 'fw_result_rule', 'fw_status_rule', 'version',
-        'ver_status')
+        'ver_status', 'created_time', 'modified_time')
 
 
 class Node_Design_Admin(admin.ModelAdmin):
     list_display = (
         'id', 'code', 'node_name', 'node_type', 'start_rule_type', 'node_func_code', 'node_func_data', 'version',
-        'ver_status')
+        'ver_status', 'created_time', 'modified_time')
 
 
 class Flow_Node_Design_Oder_Admin(admin.ModelAdmin):
     list_display = (
-        'id', 'flow_design', 'node_order', 'node_design')
+        'id', 'flow_design', 'node_order', 'node_design', 'created_time', 'modified_time')
 
 
 class Flow_Instance_Admin(admin.ModelAdmin):
     list_display = (
-        'id', 'flow_design', 'flow_data', 'flow_status', 'flow_result')
+        'id', 'flow_design', 'flow_data', 'flow_status', 'flow_result', 'created_time', 'modified_time')
 
 
 class Node_Instance_Admin(admin.ModelAdmin):
     list_display = (
         'id', 'node_design', 'node_func_name', 'node_func_data', 'node_order', 'node_status', 'node_result',
-        'flow_instance')
+        'flow_instance', 'created_time', 'modified_time')
 
 
 admin.site.register(Flow_Design, Flow_Design_Admin)
