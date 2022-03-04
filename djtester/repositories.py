86fa21061f805e9e3872c0e5b3e28b829135cdc6
wguoq <wgq1218@gmail.com>
2022-03-04 17:37:45 +0800

    优化结构

--- djtester/repositories.py
+++ djtester/repositories.py
@@ -3,18 +3,18 @@ import importlib
 from django.db import transaction
 
 
-def save_foreignkey(repositories_path, repositories_name, foreignkey_data):
-    models_ = importlib.import_module(repositories_path)
-    model = getattr(models_, repositories_name)
+def save_foreignkey(db_helper_path, db_helper_name, foreignkey_data):
+    models_ = importlib.import_module(db_helper_path)
+    db_helper = getattr(models_, db_helper_name)
     if foreignkey_data is None:
-        raise Exception(f'{foreignkey_data} foreignkey_data is None')
+        return None
     # 如果传入的是dict就保存
     elif isinstance(foreignkey_data, dict):
-        return model().save_this(foreignkey_data)
+        return db_helper().save_this(foreignkey_data)
     # 如果是int或者str就认为是pk,去查询出来
-    elif isinstance(foreignkey_data, int) or isinstance(foreignkey_data, str):
-        return model().get_by(dict(pk=foreignkey_data))
-    # 其他情况认为是obj不处理
+    elif isinstance(foreignkey_data, int or str):
+        return db_helper().get_by(dict(pk=foreignkey_data))
+    # 其他情况不处理
     else:
         return foreignkey_data
 
@@ -24,7 +24,6 @@ class BaseDBHelper:
         # 由于model.objects必须用本来的model来调用,所以import对应的model
         models = importlib.import_module(model_path)
         self.model = getattr(models, model_name)
-        self.m2m = None
 
     def get_all(self, offset: int = 0, limit: int = 1000):
         if offset < 0 or limit < 0:
@@ -40,37 +39,26 @@ class BaseDBHelper:
     def filter_by(self, kwargs: dict):
         return self.model.objects.filter(**kwargs)
 
-    @abc.abstractmethod
-    def _save_m2m(self, new_model):
+    def _set_m2m(self, new_model):
         return new_model
 
     @transaction.atomic
     def save_this(self, data: dict):
         """
-        如果model里有外键,data里面要保证对应字段是正确的obj,
-        如果有m2m的外键,要把data里对应字段移出去,单独保存后set进来
-        否则self.model(**data)就会报错
+        如果有外键要单独保存之后把对象放进data里
+        多对多的外键重写_set_m2m实现
         """
         new_model = self.model(**data)
         pk = new_model.pk
         if pk:
             # 有pk判断为修改
             # 由于update不会自动更新时间字段，还是要改成用save
-            # self.model.objects.filter(pk=pk).update(**data)
-            # new_model = self.model.objects.get(pk=pk)
-            # if self.m2m:
-            #     return self._save_m2m(new_model)
-            # else:
-            #     return new_model
             row = self.model.objects.get(pk=pk).__dict__
             row.pop('_state')
             row.update(data)
             new_model = self.model(**row)
             new_model.save()
-            return new_model
+            return self._set_m2m(new_model)
         else:
             new_model.save()
-            if self.m2m:
-                return self._save_m2m(new_model)
-            else:
-                return new_model
+            return self._set_m2m(new_model)
