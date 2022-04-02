78be79aba27795d3b0d735e7c85d16ada39cdc99
wguoq <wgq1218@gmail.com>
2022-04-02 19:08:53 +0800

    增加异常处理

--- tester/domain/tester_mgr.py
+++ tester/domain/tester_mgr.py
@@ -8,6 +8,8 @@ class TesterMgr:
 
     @staticmethod
     def run_case(test_case_pk, data_config: dict = None):
+        if TestCaseDBHelper().count_by({'pk': test_case_pk}) == 0:
+            raise Exception(f'没有查询到test_case pk = {test_case_pk}')
         test_case: Test_Case = TestCaseDBHelper().get_by_pk(test_case_pk)[0]
         if test_case.tc_type == 'api':
             r = ApiTester().run(test_case_pk, data_config)
@@ -20,18 +22,18 @@ class TesterMgr:
             raise Exception(f'无法识别的 tc_type {test_case.tc_type}')
 
 
-class NodeFuncRunApiTestCase(NodeFuncBase):
-    @reg_node_func(node_type='api_tester', class_path='tester.service')
+class RunTestCase(NodeFuncBase):
+    @reg_node_func(node_type='api_tester', class_path='tester.domain.tester_mgr')
     def __init__(self):
         pass
 
     def node_func_param(self):
-        return {'pk': ''}
+        return {'test_case_pk': ''}
 
     def node_func_result_list(self) -> list[str]:
         return ['pass', 'fail']
 
     def do_func(self, node_func_param: dict, flow_data: dict):
-        test_case_pk = node_func_param.get('pk')
+        test_case_pk = node_func_param.get('test_case_pk')
         a = TesterMgr.run_case(test_case_pk, flow_data)
         return self.NodeFuncResult(a.get('result'))
