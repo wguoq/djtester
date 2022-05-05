689e51216079c6b3801ee57c3e5b37a1caf5a645
wguoq <wgq1218@gmail.com>
2022-05-05 17:49:52 +0800

    拆分接口

--- flow/urls.py
+++ flow/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'flow'
 
 urlpatterns = [
-    # path('', views.index, name='flow_index'),
-    path('query/', views.FlowViews().query, name='query'),
-    path('commit/', views.FlowViews().commit, name='commit'),
+    path('commit', views.FlowViews().commit, name='commit'),
+    path('fields', views.FlowViews().get_fields, name='fields'),
+    path('query', views.FlowViews().query, name='query'),
+    path('save', views.FlowViews().save, name='save'),
 ]
--- flow/views.py
+++ flow/views.py
@@ -1,3 +1,8 @@
+import json
+
+from django.forms import model_to_dict
+from django.http import JsonResponse
+
 from djtester.views import BaseViews
 from flow.apps import FlowConfig
 from flow.domain.flow_mgr import FlowMgr
@@ -7,13 +12,28 @@ class FlowViews(BaseViews):
     def __init__(self):
         super().__init__(FlowConfig.name)
 
-    def _commit(self, repo: str, action: str, data: dict, condition: list = None) -> dict or list:
-        if action == "instance":
-            a = FlowMgr().instance_flow(data.get('pk'), data.get('flow_data'))
-            return dict(pk=a.pk)
-        elif action == "run":
-            a = FlowMgr().run_flow_instance(data.get('pk'))
-            return dict(pk=a.pk)
+    @staticmethod
+    def commit(request):
+        payload = json.loads(request.body) or {}
+        print(f" commit payload = {payload}")
+        action = payload.get('action')
+        data = payload.get('data')
+        if action is None or data is None:
+            return JsonResponse(dict(message='参数错误'), status=500, safe=False)
         else:
-            return super()._commit(repo, action, data, condition)
-
+            if action == 'inst':
+                try:
+                    a = FlowMgr().instance_flow(data.get('pk'), data.get('flow_data'))
+                    return JsonResponse(model_to_dict(a), status=200, safe=False)
+                except Exception as e:
+                    context = dict(message=str(e))
+                    return JsonResponse(context, status=500, safe=False)
+            elif action == 'run':
+                try:
+                    a = FlowMgr().run_flow_instance(data.get('pk'))
+                    return JsonResponse(model_to_dict(a), status=200, safe=False)
+                except Exception as e:
+                    context = dict(message=str(e))
+                    return JsonResponse(context, status=500, safe=False)
+            else:
+                return JsonResponse(dict(message='参数错误'), status=500, safe=False)
